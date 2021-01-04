cc0289491b061080d4e44cf4a2b83fda434b3e2e
Vikas Patoli <vksonpdl@gmail.com>
2021-01-04 11:39:57 +0530

    Added Base Code

---
+++ src/main/java/com/vksonpdl/expensemonitorapp/ExpenseMonitorAppApplication.java
@@ -0,0 +1,13 @@
+package com.vksonpdl.expensemonitorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseMonitorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseMonitorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vksonpdl/expensemonitorapp/ExpenseMonitorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vksonpdl.expensemonitorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseMonitorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
