b40007cfb2807976cd2b4fa9299cf0fa612b25a5
Vikas Patoli <vksonpdl@gmail.com>
2021-01-04 13:56:11 +0530

    Updated

---
+++ src/main/java/com/vksonpdl/expensemonitorapp/cntrlr/HomeController.java
@@ -0,0 +1,16 @@
+package com.vksonpdl.expensemonitorapp.cntrlr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+	
+	@GetMapping() 
+	public String getHome() {
+		
+		return "UI TBD";
+	}
+
+}
